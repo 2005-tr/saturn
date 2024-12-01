0b68ce85856c07c23a41e615b5ef0150f3225e1c
Favilances <78090594+favilances@users.noreply.github.com>
2024-12-01 13:53:51 +0300

    Update bench.py

--- bench.py
+++ bench.py
@@ -9,12 +9,12 @@ import torch
 from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
-batch_size = 12
-block_size = 1024
+batch_size = 1200
+block_size = 2300
 bias = False
 real_data = True
-seed = 1337
-device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+seed = 1375
+device = 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = True # use PyTorch 2.0 to compile the model to be faster
 profile = False # use pytorch profiler, or just simple benchmarking?
