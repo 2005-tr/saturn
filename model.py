72fff1054cd47933b0f7ff0f60a0944338740746
Favilances <melihemik@my.com>
2024-12-01 13:41:52 +0300

    update

--- model.py
+++ model.py
@@ -100,10 +100,10 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
-    n_head: int = 12
+    block_size: int = 2000
+    vocab_size: int = 20104 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 16
+    n_head: int = 19
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
